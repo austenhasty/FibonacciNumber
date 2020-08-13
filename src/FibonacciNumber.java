a359853c828063af5d75b0c720ac646861ca3e10
austenhasty <hastyae@gmail.com>
2020-08-13 13:39:59 -0700

    Simple, inefficient solution that solves the problem

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class FibonacciNumber {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        System.out.println(calcFib(n));
+    }
+
+    public static long calcFib(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return calcFib(n - 1) + calcFib(n - 2);
+        }
     }
 }
